c7fc513f179d17323c5915d8b8204d2ddab54dc8
Bonganjalo Hadebe <bonganjaloh.hadebe@gmail.com>
2021-10-21 00:05:42 +0200

    feat(service): added db operations and a condition for max number of strings

--- functions/service.ts
+++ functions/service.ts
@@ -1,6 +1,7 @@
 import * as AWS from 'aws-sdk';
 import { DynamoDB, DynamoType } from '../libs/DynamoDB';
 import { Utils } from '../libs/Utils';
+import { uuid } from 'uuidv4';
 
 let dynamodb: DynamoType;
 let utils: any;
@@ -14,10 +15,12 @@ const options = {
   region: 'eu-central-1'
 };
 
-export const handler = async (event, context, callback) => {
+export const handler = async (event, callback) => {
+
   dynamodb = dynamodb || DynamoDB(options, STAGE);
   utils = utils || Utils();
   let error:any;
+  const MAX = process.env.MAX || 3;
   const request = (event && event.body) ? { body: JSON.parse(event.body) } : {};
 
   if (!utils.isRequestValid(request)) {  // Validate request
@@ -34,7 +37,10 @@ export const handler = async (event, context, callback) => {
       data: error
     });
 
-    return {}; // .status(400).json();
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    };
   }
 
   if (!utils.isUserIdString(request)) {  // Validate request
@@ -51,13 +57,73 @@ export const handler = async (event, context, callback) => {
       data: error
     });
 
-    return {}; // response.status(400).json(error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    };
   }
 
   try {
+    const streams = await dynamodb.getStreams('active-stream-dev', request.body.userId);
+
+    console.info({
+      action: 'app:getStreams',
+      timestamp: new Date().toISOString(),
+      data: streams
+    });
+
+    if (streams.length >= MAX) {  // Check if the max number of concurrent streams is not reached
+
+      error = {
+        userId: request.body.userId,
+        error: {
+          name: 'MAX_CONCURRENT_STREAMS',
+          message: 'Client reached maximum number of concurrent streams allowed'
+        }
+      };
+
+      console.info({
+        action: 'app:max check',
+        timestamp: new Date().toISOString(),
+        data: error
+      });
+
+      return {
+        statusCode: 403,
+        body: JSON.stringify(error)
+      };
+    }
+
+    const stream = uuid();
+    streams.push(stream);
+
+    await dynamodb.saveStream('active-stream-dev', request.body.userId, streams);
+
+    console.info({
+      action: 'app:saveStream',
+      timestamp: new Date().toISOString(),
+      data: {
+        userId: request.body.userId,
+        streamId: stream
+      }
+    });
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ userId: request.body.userId, streamId: stream })
+    };
 
   } catch (err) {
-    callback(err);
+    console.info({
+      action: 'app: try catch error',
+      timestamp: new Date().toISOString(),
+      data: err
+    });
+
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    };
   }
 
 };
